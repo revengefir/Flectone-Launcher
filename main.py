7a9a0726f9a40c7c8c05a3def040dff61fe449d1
TopVovanplay <136261637+TopVovanplay@users.noreply.github.com>
2024-05-09 22:21:26 +0300

    Flectone Launcher 0.1
    The first version of launcher, it doesn't have an ui, it can only works in console.

---
+++ main.py
@@ -0,0 +1,39 @@
+import minecraft_launcher_lib
+import subprocess
+
+minecraft_directory = minecraft_launcher_lib.utils.get_minecraft_directory()
+                                                            
+print("Flectone Launcher 0.1 Alpha")
+print("by TopVovanplay")
+
+def printProgressBar(iteration,total, prefix=" ", suffix="", decimals=1, length=100, fill="█", printEnd="\r"):
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix), end=printEnd)
+    if iteration == total:
+        print()
+
+def maximum(max_value, value):
+    max_value[0] = value
+
+version = input("Выберите версию: ")
+username = input("username: ")
+print("=======================================================================================")
+
+max_value = [0]
+callback = {
+    "setStatus": lambda text: print(text, end="r"),
+    "setProgress": lambda value: printProgressBar(value, max_value[0]),
+    "setMax": lambda value: maximum(max_value, value)
+}
+
+minecraft_launcher_lib.install.install_minecraft_version(versionid=version, minecraft_directory=minecraft_directory, callback=callback)
+
+options = {
+    "username": username,
+}
+
+subprocess.call(minecraft_launcher_lib.command.get_minecraft_command(version=version, minecraft_directory=minecraft_directory, options=options))
+                                                                
+                                                                
